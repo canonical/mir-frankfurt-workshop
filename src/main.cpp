0f2c04e6937c2337fef49fba3b44b7fa78d0fc96
Matthew Kosarek <matt.kosarek@canonical.com>
2025-04-28 16:03:44 -0400

    Implementation opening a terminal

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,54 @@
 #include <miral/runner.h>
 #include <miral/set_window_management_policy.h>
 #include <miral/minimal_window_manager.h>
+#include <miral/external_client.h>
+#include <miral/append_event_filter.h>
+#include <miral/toolkit_event.h>
+#include <xkbcommon/xkbcommon-keysyms.h>
 
 int main(int argc, char const* argv[])
 {
    mir::log_info("Running Mirly");
    miral::MirRunner runner{argc, argv};
+   miral::ExternalClientLauncher external_client_launcher;
+
+   auto const open_terminal = [&](MirEvent const* event)
+    {
+        if (miral::toolkit::mir_event_get_type(event) != mir_event_type_input)
+           return false;
+
+        MirInputEvent const* input_event =  
+            miral::toolkit::mir_event_get_input_event(event);
+        if (miral::toolkit::mir_input_event_get_type(input_event)
+               != mir_input_event_type_key)
+            return false;
+
+        MirKeyboardEvent const* kev =  miral::toolkit::mir_input_event_get_keyboard_event(input_event);
+
+        if (miral::toolkit::mir_keyboard_event_action(kev) != mir_keyboard_action_down)
+          return false;
+
+        MirInputEventModifiers mods = miral::toolkit::mir_keyboard_event_modifiers(kev);
+        if (!(mods & mir_input_event_modifier_alt) || !(mods & mir_input_event_modifier_ctrl))
+            return false;
+  
+        std::string terminal_cmd = "konsole"; // Replace with your terminal command
+        switch (miral::toolkit::mir_keyboard_event_keysym(kev))
+        {
+            case XKB_KEY_t:
+            case XKB_KEY_T:
+                external_client_launcher.launch({terminal_cmd});
+                return true;
+            default:
+                return false;
+        }
+    };
 
    return runner.run_with(
     {
-        miral::set_window_management_policy<miral::MinimalWindowManager>()
+        miral::set_window_management_policy<miral::MinimalWindowManager>(),
+        external_client_launcher,
+        miral::AppendEventFilter(open_terminal)
     });
 
 }
