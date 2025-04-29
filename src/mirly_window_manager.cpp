e275e41e71346509de2dfa43b5490cfdee4018c9
Matthew Kosarek <matt.kosarek@canonical.com>
2025-04-29 15:24:34 -0400

    Implement ability to center a window using a keybind

--- src/mirly_window_manager.cpp
+++ src/mirly_window_manager.cpp
@@ -7,6 +7,18 @@
 #include <miral/toolkit_event.h>
 #include <xkbcommon/xkbcommon-keysyms.h>
 
+namespace
+{
+class ExtraWindowData
+{
+public:
+    ExtraWindowData(mir::geometry::Rectangle const& placement)
+        : original_placement(placement) {}
+    mir::geometry::Rectangle const original_placement;
+    bool is_centered = false;
+};
+}
+
 MirlyWindowManager::MirlyWindowManager(miral::WindowManagerTools const& tools)
     : miral::MinimalWindowManager(tools)
 {
@@ -23,10 +35,44 @@ bool MirlyWindowManager::handle_keyboard_event(MirKeyboardEvent const* event)
 
     switch (miral::toolkit::mir_keyboard_event_keysym(event))
     {
+        //
         case XKB_KEY_q:
         case XKB_KEY_Q:
             tools.ask_client_to_close(tools.active_window());
             return true;
+        case XKB_KEY_c:
+        case XKB_KEY_C:
+        {
+            auto const active_window = tools.active_window();
+            if (!active_window)
+                break;
+
+            auto const& info = tools.info_for(active_window);
+            auto extra_data = std::static_pointer_cast<ExtraWindowData>(info.userdata());
+            if (!extra_data)
+                break;
+
+            miral::WindowSpecification specification;
+            if (extra_data->is_centered)
+            {
+                extra_data->is_centered = false;
+                specification.top_left() = extra_data->original_placement.top_left;
+                specification.size() = extra_data->original_placement.size;
+            }
+            else
+            {
+                auto const placement_rectangle = tools.active_output();
+                int const center_x = placement_rectangle.top_left.x.as_value() + placement_rectangle.size.width.as_int() / 2;
+                int const center_y = placement_rectangle.top_left.y.as_value() + placement_rectangle.size.height.as_int() / 2;
+                int const placement_x = center_x - active_window.size().width.as_int() / 2;
+                int const placement_y = center_y - active_window.size().height.as_int() / 2;
+                extra_data->is_centered = true;
+                specification.top_left() = mir::geometry::Point(placement_x, placement_y);
+            }
+
+            tools.modify_window(active_window, specification);
+            return true;
+        }
         default:
             return false;
     }
@@ -63,5 +109,6 @@ auto MirlyWindowManager::place_new_window(
     miral::WindowSpecification specification = requested_specification;
     specification.top_left() = placement.top_left;
     specification.size() = placement.size;
+    specification.userdata() = std::make_shared<ExtraWindowData>(placement);
     return specification;
 }
